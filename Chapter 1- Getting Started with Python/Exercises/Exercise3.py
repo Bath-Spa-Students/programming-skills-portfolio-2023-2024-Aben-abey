c580585ce48e5f2338b763d108ce88180783c829
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-03-11 19:14:54 +0400

    Create Exercise3.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise3.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+# To get the current date and time
+now = datetime.now()
+
+# Displaying the current date and time using the command now
+print("Current date and time: ", now) 
