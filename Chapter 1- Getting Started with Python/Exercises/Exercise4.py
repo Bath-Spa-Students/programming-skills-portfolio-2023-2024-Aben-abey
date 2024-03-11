45e107e0567b23bde0f3d6b16a1460f12455eebe
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-03-11 19:15:04 +0400

    Create Exercise4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,8 @@
+# Define three strings in different variables
+string1 = "Hello"
+string2 = "My name is aben"
+string3 = "I stay in sharjah"
+
+# Concatenate the strings and print the output
+output_string = string1 + " " + string2 + " " + string3
+print(output_string)
