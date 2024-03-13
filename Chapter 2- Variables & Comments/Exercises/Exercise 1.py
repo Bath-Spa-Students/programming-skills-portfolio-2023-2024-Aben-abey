c13276f817774555beb36d9410aa54b26dcf55f6
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-03-13 11:26:09 +0400

    Create Exercise 1.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise 1.py
@@ -0,0 +1,11 @@
+# Assign a message to a variable
+message = "I love learning Python it is very essential in todays World ."
+
+# Print the message
+print(message)
+
+# Change the value of the variable to a new message
+message = "It's really satisfying To learn new things!"
+
+# Print the new message
+print(message)
