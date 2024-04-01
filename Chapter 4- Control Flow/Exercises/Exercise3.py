8518da15e2cb8014adbf408a011345b39c866614
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:49:19 +0400

    Create Exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -0,0 +1,11 @@
+# Define the color of the alien
+alien_color = 'green'
+
+# Check the color of the alien and print the appropriate message
+if alien_color == 'green':
+    print("The player earned 5 points.")
+elif alien_color == 'yellow':
+    print("The player earned 10 points.")
+else:
+    print("The player earned 15 points.")
+
