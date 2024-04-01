d7aacafe922cb3d9553c317551e3e2f83f531aa1
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:28:15 +0400

    Create Exercise4.py
    Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,6 @@
+# Make a list of guests
+guests = ['Edwin Abey', 'jincy Joy', 'Beiji Bijoy']
+
+# Print a message to each person inviting them to dinner
+for guest in guests:
+    print(guest + ",\n    please come to dinner.")
