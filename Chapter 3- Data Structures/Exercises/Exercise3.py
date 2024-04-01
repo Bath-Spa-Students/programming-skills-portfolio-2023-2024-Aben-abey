125b9b857d706bd39af1d796b01ac4b5d8b64475
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:27:05 +0400

    Exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,4 @@
+names=["motorcycle","car","train"]
+print ( "i prefer to travel in" ,names[0], "to school")
+print ( "i prefer to travel in" ,names[1], "to school")
+print ( "i prefer to travel in" ,names[2], "to school")
