fde8e1dcac48c5b6da4682c842d41450be9761f9
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:50:59 +0400

    Create Exercise4.py

---
+++ Chapter 7- Functions/Exercises/Exercise4.py
@@ -0,0 +1,11 @@
+def make_shirt(size='large', message='I love Python'):
+    print(f"Making a {size} shirt with the message: {message}")
+
+# Make a large shirt with the default message
+make_shirt()
+
+# Make a medium shirt with the default message
+make_shirt(size='medium')
+
+# Make a custom-sized shirt with a different message
+make_shirt(size='small', message='Keep Coding!')
