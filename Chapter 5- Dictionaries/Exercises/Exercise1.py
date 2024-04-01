51bc636052d8acc1d71c9865817e5fb51764c5a7
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:49:41 +0400

    Create Exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,13 @@
+# Make a dictionary to store information about a person.
+person = {
+    'first_name': 'Aben',
+    'last_name': 'Abey',
+    'age': 19,
+    'city': 'Abu Shagara'
+}
+
+# Print the information stored in the dictionary.
+print("First name: " + person['first_name'])
+print("Last name: " + person['last_name'])
+print("Age: " + str(person['age']))
+print("City: " + person['city'])
