f546e0b9a4ff6a077ad312b1e31aff09e281352d
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:26:01 +0400

    Exercise1.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise1.py
@@ -0,0 +1,6 @@
+# Define the list of names
+names = ["Mervyn", "Alon", "Alan", "Akku", "Abdu"]
+
+# Print each person's name by accessing each element in the list
+for name in names:
+    print(name)
