0563e440f05b976f8975db0439854a8fe2e02f6f
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:49:14 +0400

    Create Exercise2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise2.py
@@ -0,0 +1,10 @@
+# Choose a color for the alien
+alien_color = 'yellow'
+
+# Check if the alien's color is green
+if alien_color == 'green':
+    # If the color is green, print a message and award 5 points
+    print("You just earned 5 points for shooting the alien.")
+else:
+    # If the color is not green, print a message and award 10 points
+    print("You just earned 10 points for shooting the alien.")
