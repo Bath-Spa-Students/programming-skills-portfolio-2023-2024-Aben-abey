c305b30277efa30ba97a280563b17634997812cb
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:50:56 +0400

    Create exercise 3.py

---
+++ Chapter 7- Functions/Exercises/exercise 3.py
@@ -0,0 +1,8 @@
+def make_shirt(size, message):
+    print(f"Size: {size}, Message: {message}")
+
+# Calling the function with positional arguments
+make_shirt("Medium", "Hello, World!")
+
+# Calling the function with keyword arguments
+make_shirt(size="Large", message="Python is fun!")
