19173077c6e6627f31b65dd485f3793cfbe383c1
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:26:30 +0400

    Exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,7 @@
+# Define the list of names
+names = ["Mervyn", "Alon", "Alan", "Akku", "Abdu"]
+
+# Print a personalized message to each person
+for name in names:
+    message = f"Hello, {name}! I hope you're having a nice day."
+    print(message)
