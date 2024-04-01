483f57b91b1ce7570a27bd547af1cce913624f73
Aben-abey <158697343+Aben-abey@users.noreply.github.com>
2024-04-01 14:50:48 +0400

    Create Exercise 2.py

---
+++ Chapter 7- Functions/Exercises/Exercise 2.py
@@ -0,0 +1,5 @@
+def favorite_book(title):
+    print(f"My favorite book is '{title}'.")
+
+# Call the function and pass the title of your favorite book as an argument
+favorite_book("To Kill a Mockingbird")
